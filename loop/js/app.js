39faaca3b4f5b64ecacc5ec8605bcd62ff3c5969
Jun <jun55tsuno@gmail.com>
2022-09-08 17:39:38 -0700

    finished project

--- loop/js/app.js
+++ loop/js/app.js
@@ -1,9 +1,24 @@
 const $odd = $('a:odd');
 const $secureLinks = $('a[href^="https://"]');
 const $pdf = $('a[href$=".pdf"]');
+const $pdfCheckbox = $('<label><input type="checkbox"> Allow PDF downloads</label>');
 
 $secureLinks.attr('target', '_blank');
 $pdf.attr('download', true);
 
 $secureLinks.addClass('secure');
 $pdf.addClass('pdf');
+
+$pdf.on('click', function(e) {
+    if($(':checked').length === 0) {
+        e.preventDefault();
+        alert('Please check the checkbox.');
+    }
+});
+
+$('#links').append($pdfCheckbox);
+
+$('a').each(function() {
+    const url = $(this).attr('href');
+    $(this).parent().append(` (${url})`);
+})
